7670d72c59bfb38fdff6d96f26d90faa59dcffef
rafiqul.islam <rafiqulislam.mca18.du@gmail.com>
2023-01-04 15:21:22 +0530

    this run-server

--- py_clients/basic.py
+++ py_clients/basic.py
@@ -2,8 +2,8 @@ import requests
 
 
 endpoint ="https://httpbin.org/anything"
-endpoint1="https://httpbin.org/anything"
-get_response=requests.get(endpoint1,json={"query":"Hello world"})# i am getting some response from this websites
+endpoint1="http://localhost:8000/"   #"http://127.0.0.1:8000/"
+get_response=requests.get(endpoint1)# i am getting some response from this websites
 
 #print as text file
-print(get_response.json())
+print(get_response.text)
