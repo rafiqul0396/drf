13514e1151358ba7f7cbd739750eb2d7307962d3
rafiqul.islam <rafiqulislam.mca18.du@gmail.com>
2023-01-04 17:05:16 +0530

    this procuts first commit

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,19 @@
 from django.http import  JsonResponse
-
+import json
 
 def  api_home(request,*args, **kwargs):
+
+    body=request.body # byte string of Json Data
+    print(request.GET)
+    print(request.POST)
+    
+    data={}
+    try:
+        data=json.loads(body)
+    except :
+        pass
+
+    print(data.keys())
     return JsonResponse({"message":"hi, there any on take my response"})
+
 # Create your views here.
--- py_clients/basic.py
+++ py_clients/basic.py
@@ -1,10 +1,11 @@
 import requests 
 
 
+
 endpoint ="https://httpbin.org/anything"
 endpoint1="http://localhost:8000/api/"   #"http://127.0.0.1:8000/"
-get_response=requests.get(endpoint1)# i am getting some response from this websites
+get_response=requests.get(endpoint1,params={"abc":123},json={"query":"how are you"})# i am getting some response from this websites
 
 #print as text file
-print(get_response.text)
-print(get_response.json()["message"])
+#print(get_response.text)
+print(get_response.json())
